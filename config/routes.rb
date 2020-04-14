d0f12992026848b819e8a13b74736943a3995995
Juan de Dios Gomez F <jddios837@gmail.com>
2020-04-14 10:18:40 -0500

    Create blog scaffold & controller pages

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :blogs
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
+  resources :high_scores
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
